04177f6c4be9e0852927359338c97d5a5885d836
Vitalii <crossdoh@gmail.com>
2025-01-16 14:00:49 +0200

    PC-22: Add spec to the Customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:company_name) }
+    it { should validate_uniqueness_of(:company_name) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:position) }
+    it { should validate_presence_of(:address) }
+  end
 end
