2d0dacb905246ab673bc3bf9e7dcec8d288d8175
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-01-16 10:22:56 +0200

    PC-17: Remove unused configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
-  end
 end
