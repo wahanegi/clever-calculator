715488de920ea42d552794b77d1c4bc2e9c89b00
Vitalii Bakun <crossdoh@gmail.com>
2025-01-16 17:43:20 +0200

    PC-22: Add validation to email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,9 @@
 class Customer < ApplicationRecord
   validates :company_name, presence: true, uniqueness: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
   validates :address, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :position, presence: true
+  validates :notes, presence: true
 end
