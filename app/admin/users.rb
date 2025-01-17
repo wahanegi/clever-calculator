63dff5d3cc28f70028b30bb37482f2a88f859982
AndrewRiabets <79791263+AndrewRiabets@users.noreply.github.com>
2025-01-17 11:58:33 +0200

    update admin

---
+++ app/admin/users.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register User do
+  permit_params :email, :name, :password, :password_confirmation
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :name
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
