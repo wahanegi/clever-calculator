b4014aa61cef734d2deac7d513edbf1941d27130
AndrewRiabets <79791263+AndrewRiabets@users.noreply.github.com>
2025-01-17 12:25:38 +0200

    fix lint

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,7 +4,7 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     %w[id email created_at updated_at]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
