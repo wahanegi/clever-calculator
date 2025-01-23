6475233a9ecce838e7ba87981a8ec4d537b5e313
AndrewRiabets <79791263+AndrewRiabets@users.noreply.github.com>
2025-01-23 16:32:29 +0200

    Upgraded tests

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :admin_user do
     email { Faker::Internet.unique.email }
-    name { Faker::Internet.username.gsub(/[^a-zA-Z0-9_]/, '') }
+    name { Faker::Name.name }
     password { '@password' }
     password_confirmation { '@password' }
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
-    name { Faker::Internet.username.gsub(/[^a-zA-Z0-9_]/, '') }
+    name { Faker::Name.name }
     password { '@password' }
     password_confirmation { '@password' }
   end
