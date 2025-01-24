48e3d99b281d9f492ea24d671d7307e81f1fb2ee
Vitalii Bakun <crossdoh@gmail.com>
2025-01-24 21:29:04 +0200

    PC-22: Remove validates in Customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'validations' do
     let(:customer) { build(:customer) }
+
     it('should be valid') { expect(customer).to be_valid }
 
     describe 'company_name' do
@@ -18,24 +19,5 @@ RSpec.describe Customer, type: :model do
         expect(customer.errors[:company_name]).to include('has already been taken')
       end
     end
-
-    describe 'email' do
-      let(:customer) { build(:customer) }
-
-      it { should validate_presence_of(:email) }
-
-      it('should not allow invalid emails') do
-        ['bob12', 'bob@gmail,com', 'bob@.com', 'bob12 bob@gmail.com'].each do |invalid_email|
-          customer.email = invalid_email
-          expect(customer).not_to be_valid
-          expect(customer.errors[:email]).to include('must be a valid email address')
-        end
-      end
-    end
-
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:position) }
-    it { should validate_presence_of(:address) }
   end
 end
