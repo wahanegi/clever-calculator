389ee56a67b8daa351fdbd2de0855acbd64234df
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-03-10 14:03:10 +0000

    fix: update files after RuboCop check

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true,
-            format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
   validates :password, length: { minimum: 8, maximum: 128 },
-            format: { with: PASSWORD_SYMBOL_FORMAT, message: "must contain at least one symbol" },
-            presence: true, on: :create
+                       format: { with: PASSWORD_SYMBOL_FORMAT, message: "must contain at least one symbol" },
+                       presence: true, on: :create
   validates :password, format: { with: PASSWORD_REPEATED_CHAR_FORMAT, message: "must not contain repeated characters" }
 
   def self.ransackable_attributes(_auth_object = nil)
