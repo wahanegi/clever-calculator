fb7df30142af27f929606b9392cb3cc58c6d10ab
AndrewRiabets <79791263+AndrewRiabets@users.noreply.github.com>
2025-03-11 12:11:09 +0200

    PC-53 Add Item Tab in ActiveAdmin

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
     validates :name, presence: true 
     validates :pricing_type, presence: true
     validates :name, uniqueness: { scope: :category_id, message: "Item name must be unique within category" }
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["category_id", "created_at", "id", "is_disable", "name", "pricing_type", "updated_at"]
+    end
+    
+    def self.ransackable_associations(auth_object = nil)
+        ["category"]
+    end
 end
