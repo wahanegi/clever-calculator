6d3e9744513382cf6fa61bbea0a22e8afa1b45c9
Vitalii Bakun <crossdoh@gmail.com>
2025-03-12 11:45:47 +0200

    PC-45 Add Category View Page

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -11,13 +11,31 @@ ActiveAdmin.register Category do
     column :created_at
     column :updated_at
     actions defaults: false do |category|
-      item 'View', admin_category_path(id: category.id), class: 'member_link'
-      item 'Edit', edit_admin_category_path(id: category.id), class: 'member_link'
-      if category.is_disabled
-        item 'Disable', admin_category_path(id: category.id, category: { is_disabled: false }),
-             method: :put,
+      item 'View', admin_category_path(category), class: 'member_link'
+      item 'Edit', edit_admin_category_path(category), class: 'member_link'
+      unless category.is_disabled
+        item 'Disable', disable_admin_category_path(category), method: :put,
              class: 'member_link'
       end
+    end
+  end
+
+  # Remove the destroy button only on the show page
+  config.action_items.delete_if { |item| item.name == :destroy && item.display_on?(:show) }
+
+  action_item :disable, only: :show, if: -> { !resource.is_disabled? } do
+    link_to "Disable Category", disable_admin_category_path(resource), method: :put
+  end
+
+  action_item :back, only: :show do
+    link_to "Back", admin_categories_path
+  end
+
+  member_action :disable, method: :put do
+    if resource.update(is_disabled: true)
+      redirect_to admin_categories_path, notice: 'Category was successfully disabled.'
+    else
+      redirect_to admin_category_path(resource), notice: resource.errors
     end
   end
 end
