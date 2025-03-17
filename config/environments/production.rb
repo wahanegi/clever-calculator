8fe9fa14f08fa8aabd79c59ec80f0b47490c846f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-03-17 16:35:02 +0200

    PC-65: Update configuration for solid_cache and solid_queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,11 +48,9 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
-  config.solid_cache.connects_to = { database: { writing: :primary } }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :primary } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
