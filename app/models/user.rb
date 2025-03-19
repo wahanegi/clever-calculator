d5c29acc525a46aae02fca31b7beb9c67e991365
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-03-19 16:15:41 +0000

    PC-30: update password length check for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable
   PASSWORD_SYMBOL_FORMAT = /\A(?=.*[^\w\s])[^\s]*\z/
   PASSWORD_REPEATED_CHAR_FORMAT = /\A(?!.*(.)\1\1).*\z/
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
   validates :password, length: { minimum: 8, maximum: 128 },
-                       format: { with: PASSWORD_SYMBOL_FORMAT, message: "must contain at least one symbol" },
-                       presence: true, on: :create
+            format: { with: PASSWORD_SYMBOL_FORMAT, message: "must contain at least one symbol" },
+            presence: true, on: :create
   validates :password, format: { with: PASSWORD_REPEATED_CHAR_FORMAT, message: "must not contain repeated characters" }
 
   def self.ransackable_attributes(_auth_object = nil)
