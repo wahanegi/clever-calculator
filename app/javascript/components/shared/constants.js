f86f03b135267986d5fa917757ea74e17b404a7d
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-03-19 07:35:44 +0000

    PC-62: install axios & create axiosInstance.js, httpRequests.js service, fetchService for quotes

--- app/javascript/components/shared/constants.js
+++ app/javascript/components/shared/constants.js
@@ -1,6 +1,10 @@
 export const ROUTES = {
-    HOME: '/',
-    CUSTOMER_INFO: '/customer-info',
-    ITEM_PRICING: '/items-pricing',
-    NOT_FOUND: '*',
+  HOME: '/',
+  CUSTOMER_INFO: '/customer-info',
+  ITEM_PRICING: '/items-pricing',
+  NOT_FOUND: '*',
+}
+
+export const ENDPOINTS = {
+  QUOTES: '/quotes',
 }
