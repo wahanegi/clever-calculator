c4b01842715814b05b5dd5171f8f17954bc567b6
Vitalii Bakun <crossdoh@gmail.com>
2025-03-21 14:42:26 +0200

    PC-76 Add new filters to the string input

---
+++ config/initializers/active_admin_filters_string_input.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+module ActiveAdmin
+  module Inputs
+    module Filters
+      class StringInput
+        # Adding new default filters to the string input
+        filter :matches
+      end
+    end
+  end
+end
