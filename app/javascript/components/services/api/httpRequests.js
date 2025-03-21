120568aea2c4cf18828771d0207494f3d4de4509
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-03-21 10:27:31 +0000

    PC-59: add default values to the request function to prevent potential errors.

--- app/javascript/components/services/api/httpRequests.js
+++ app/javascript/components/services/api/httpRequests.js
@@ -3,7 +3,7 @@ import { instance } from './axiosInstance'
 // Function to make HTTP requests & using try/catch for error handling
 const request = async (method = 'GET', url = '', data = {}, config = {}) => {
   try {
-    const response = await instance({ method, url, data, ...config })
+    const response = await instance({method, url, data, ...config})
     return response?.data
   } catch (err) {
     console.error(`API Error (${method} ${url}):`, err.response || err)
@@ -12,8 +12,8 @@ const request = async (method = 'GET', url = '', data = {}, config = {}) => {
 }
 
 // HTTP requests functions
-export const get = (url, config) => request(url, config)
+export const get = (url, config) => request('GET', url, {}, config)
 export const post = (url, data, config) => request('POST', url, data, config)
 export const put = (url, data, config) => request('PUT', url, data, config)
 export const patch = (url, data, config) => request('PATCH', url, data, config)
-export const del = (url, config) => request('DELETE', url, config)
+export const del = (url, config) => request('DELETE', url, {}, config)
