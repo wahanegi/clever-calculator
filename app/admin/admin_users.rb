9122acb59b7b7341722e5fdf3669d60722c958b5
Vitalii Bakun <crossdoh@gmail.com>
2025-03-21 17:04:37 +0200

    PC-67 Change the order of inputs

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -29,8 +29,8 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs do
-      f.input :email
       f.input :name
+      f.input :email
       f.input :password, required: resource.new_record?
     end
     f.actions
