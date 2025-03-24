97d1065d248054eb92ddb4ebd60ec9f1d4bf7d66
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-03-24 12:32:14 +0200

    PC-71: Updated mail messages

--- app/assets/javascripts/password_toggle.js
+++ app/assets/javascripts/password_toggle.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const passwordField = document.getElementById('password-field')
-  const togglePassword = document.getElementById('eye-icon')
+  const togglePassword = document.getElementById('toggle-password')
+  const eyeIcon = document.getElementById('eye-icon')
 
-  if (passwordField && togglePassword) {
+  if (passwordField && togglePassword && eyeIcon) {
     togglePassword.addEventListener('click', function () {
-      // Toggle the type attribute
-      const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password'
-      passwordField.setAttribute('type', type)
+      // Toggle password visibility
+      const isPasswordHidden = passwordField.getAttribute('type') === 'password'
+      passwordField.setAttribute('type', isPasswordHidden ? 'text' : 'password')
 
-      // Toggle the icon's appearance
-      togglePassword.classList.toggle('password-visible')
+      // Toggle between show and hide icons
+      const newIcon = isPasswordHidden ? 'eye_hide_icon.svg' : 'eye_show_icon.svg'
+      eyeIcon.src = `/assets/${newIcon}`
     })
   }
 })
