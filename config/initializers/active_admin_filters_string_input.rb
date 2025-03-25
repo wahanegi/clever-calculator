20f0b426dc10b0cb0c36e07d9d4a12423177b5d9
Vitalii Bakun <crossdoh@gmail.com>
2025-03-25 17:56:37 +0200

    PC-76 Update I18n text for filters and remove 'equals' from string input

--- config/initializers/active_admin_filters_string_input.rb
+++ config/initializers/active_admin_filters_string_input.rb
@@ -4,6 +4,8 @@ module ActiveAdmin
       class StringInput
         # Adding new default filters to the string input
         filter :matches
+        # Removing the default eq filter
+        filters.delete(:eq)
       end
     end
   end
