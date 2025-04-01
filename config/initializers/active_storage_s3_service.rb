723349b7b02dd242894da40efc92c57764ac5acf
Vitalii Bakun <crossdoh@gmail.com>
2025-04-01 20:11:56 +0300

    PC-90 Use class_eval to modify the S3Service class

--- config/initializers/active_storage_s3_service.rb
+++ config/initializers/active_storage_s3_service.rb
@@ -1,12 +1,7 @@
 require 'active_storage/service/s3_service'
-require 'active_storage/service'
 
-module ActiveStorage
-  class Service
-    class S3Service < ActiveStorage::Service
-      def url(key, **_options)
-        "#{ENV['AWS_FILES_DISTRIBUTION']}/#{key}"
-      end
-    end
+ActiveStorage::Service::S3Service.class_eval do
+  def url(key, **_options)
+    "#{ENV['AWS_FILES_DISTRIBUTION']}/#{key}"
   end
 end
