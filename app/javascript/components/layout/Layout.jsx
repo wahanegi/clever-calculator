049d088bb20b438805e15b0e914eacbbcc5b61d9
Vitalii Bakun <crossdoh@gmail.com>
2025-04-04 16:21:03 +0300

    PC-99 Fix background image in Layout.jsx

--- app/javascript/components/layout/Layout.jsx
+++ app/javascript/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
   return (
-    <div className={'d-flex flex-column min-vh-100'}>
+    <div className={'layout d-flex flex-column min-vh-100'}>
       <Header />
       <main>
         <Outlet />
