7a6fe09ebdeb5af5727a49747e6cfb4c9ddcdcbc
Vitalii Bakun <crossdoh@gmail.com>
2025-04-22 22:43:24 +0300

    PC-111: Fix bug where chevron icon is not clickable and does not display drop-down menu

--- app/javascript/components/ui/PcDropdownSelect.jsx
+++ app/javascript/components/ui/PcDropdownSelect.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Form } from 'react-bootstrap'
 import { Typeahead } from 'react-bootstrap-typeahead'
-import { PcIcon } from './PcIcon'
 import { normalizeName } from '../utils'
 
 export const PcDropdownSelect = ({
@@ -28,32 +27,38 @@ export const PcDropdownSelect = ({
     return options.some((option) => normalizeName(option.label) === normalizedInput)
   }
 
+  const handleChange = (selected) => {
+    if (selected.length > 0) {
+      const selectedOption = selected[0]
+      const valueToPass = selectedOption.customOption ? selectedOption.label : selectedOption.value
+      onChange({ target: { id, value: valueToPass } })
+    } else {
+      onChange({ target: { id, value: '' } })
+    }
+  }
+
+  const handleInputChange = (text, event) => {
+    setInputValue(text)
+    if (onInputChange) onInputChange(event)
+  }
+
+  const handleFilterBy = (option, props) => {
+    const inputValue = normalizeName(props.text)
+    return normalizeName(option.label).includes(inputValue)
+  }
+
   return (
-    <Form.Group controlId={id} className="pc-typeahead-customer-info position-relative">
+    <Form.Group controlId={id} className="pc-typeahead position-relative">
       <Typeahead
         id={id}
         options={options}
         placeholder={placeholder}
-        onChange={(selected) => {
-          if (selected.length > 0) {
-            const selectedOption = selected[0]
-            const valueToPass = selectedOption.customOption ? selectedOption.label : selectedOption.value
-            onChange({ target: { id, value: valueToPass } })
-          } else {
-            onChange({ target: { id, value: '' } })
-          }
-        }}
-        onInputChange={(text, event) => {
-          setInputValue(text)
-          if (onInputChange) onInputChange(event)
-        }}
-        filterBy={(option, props) => {
-          const inputValue = normalizeName(props.text)
-          return normalizeName(option.label).includes(inputValue)
-        }}
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        filterBy={handleFilterBy}
         allowNew={!hasMatchingOption()}
         newSelectionPrefix="Add new customer: "
-        className="pc-icon-reserve-place border border-primary rounded-1"
+        className={`${hasIcon ? isMenuOpen ? 'pc-typeahead-arrow-up' : 'pc-typeahead-arrow-down' : ''} border border-primary rounded-1`}
         style={{ height }}
         selected={selectedOption ? [selectedOption] : []}
         maxResults={maxResults}
@@ -61,14 +66,6 @@ export const PcDropdownSelect = ({
         onMenuToggle={(isOpen) => setIsMenuOpen(isOpen)}
       />
       <Form.Label className="border-label fw-bold fs-11 lh-sm px-1">{label}</Form.Label>
-      {hasIcon && (
-        <div className="position-absolute end-0 top-50 translate-middle-y pe-4 pb-1">
-          <PcIcon
-            name={isMenuOpen ? 'dropdownArrowUp' : 'dropdownArrowDown'}
-            alt={isMenuOpen ? 'Arrow pointing up' : 'Arrow pointing down'}
-          />
-        </div>
-      )}
       {error && <div className="text-danger fs-12">{error}</div>}
     </Form.Group>
   )
