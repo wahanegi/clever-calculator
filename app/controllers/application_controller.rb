c07671e61dee97dd384f3b3edf1e91889a0df41c
Vitalii Bakun <crossdoh@gmail.com>
2025-05-05 13:08:46 +0300

    PC-140: Add CSRF error handling for ActiveAdmin::BaseController and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :active_admin_controller?
   before_action :set_active_storage_url_options
 
+  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_user_csrf_error
+
   private
+
+  def handle_user_csrf_error
+    redirect_to new_user_session_path, alert: "Your session has expired. Please sign in again."
+  end
 
   def active_admin_controller?
     is_a?(ActiveAdmin::BaseController)
