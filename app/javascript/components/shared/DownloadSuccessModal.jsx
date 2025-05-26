40fc3b2b2f7d95971e3ee8ff2a79ba2daf27d311
Vitalii Bakun <crossdoh@gmail.com>
2025-05-26 15:38:43 +0300

    PC-138: Update modal padding and margin

--- app/javascript/components/shared/DownloadSuccessModal.jsx
+++ app/javascript/components/shared/DownloadSuccessModal.jsx
@@ -9,17 +9,17 @@ export const DownloadSuccessModal = ({ show, onHide, props }) => {
            show={show}
            onHide={onHide}
            dialogClassName={'pc-mw-524'}
-           contentClassName={'border-1 border-primary p-4 p-xxl-8 p-xl-6 bg-light'}
+           contentClassName={'border-1 border-primary p-4 p-xxl-7 p-xl-5 bg-light'}
            {...props}>
       <Modal.Header closeButton className={'p-0 border-0'} />
-      <Modal.Body className={'d-flex flex-column align-items-center justify-content-center p-0 mt-3 mt-xxl-9 mt-xl-6'}>
-        <PcIcon name={'checkCircle'} className={'mb-3 mb-xxl-10 mb-xl-7 mb-lg-5'} />
-        <h2 className={'mb-5 mb-xxl-17 mb-xl-10 mb-lg-8 fw-bold text-center'}>
+      <Modal.Body className={'d-flex flex-column align-items-center justify-content-center p-0 mt-3 mt-xxl-6 mt-xl-4'}>
+        <PcIcon name={'checkCircle'} className={'mb-3 mb-xxl-7 mb-xl-5 mb-lg-4'} />
+        <h2 className={'mb-5 mb-xxl-10 mb-xl-7 mb-lg-5 fw-bold text-center'}>
           Your file downloaded
-         <div className={'text-primary mt-3 mt-xxl-7 mt-xl-4'}>successfully</div>
+         <div className={'text-primary mt-3 mt-xxl-5 mt-xl-4'}>successfully</div>
         </h2>
 
-        <div className={'mb-3 mb-xxl-12 mb-xl-7 mb-lg-5'}>
+        <div className={'mb-3 mb-xxl-8 mb-xl-6 mb-lg-5'}>
           <img src={IMAGE_ASSETS.BACKGROUNDS.manCalculator} className={'img-fluid'} alt={'Downloaded file'} />
         </div>
       </Modal.Body>
