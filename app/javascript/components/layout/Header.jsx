33fac28c4d6284f04cde6ce3478186eae88fb4e2
Vitalii Bakun <crossdoh@gmail.com>
2025-06-13 12:16:31 +0300

    PC-181: Remove text from the logo component

--- app/javascript/components/layout/Header.jsx
+++ app/javascript/components/layout/Header.jsx
@@ -9,13 +9,7 @@ export const Header = ({ setting }) => {
     <Navbar.Brand className={'ms-1 ms-xxl-27 ms-xl-27 ms-lg-10 ms-md-2'}>
       {setting?.app_logo_icon ?
         (<img src={setting.app_logo_icon} className={'header-logo object-fit-contain'} alt={'Logo'} />)
-        : (<div className={'header-logo d-flex'}>
-          <PcIcon name={'logo'} className={'pc-icon-logo header-logo-separator'} alt={'Logo'} />
-          <div className={'header-logo-text d-flex flex-column'}>
-            <span className={'fw-bold text-uppercase text-black'}>This is</span>
-            <span className={'fw-bolder text-uppercase text-black'}>My logo</span>
-          </div>
-        </div>)}
+        : (<PcIcon name={'logo'} className={'header-logo pc-icon-logo'} alt={'Logo'} />)}
     </Navbar.Brand>
 
   const UserProfile = () => {
@@ -28,7 +22,8 @@ export const Header = ({ setting }) => {
       }
     }
 
-    return <Button onClick={handleLogout} className={'me-1 me-xxl-25 me-xl-25 me-lg-15 me-md-2 header-logout btn btn-primary'}>
+    return <Button onClick={handleLogout}
+                   className={'me-1 me-xxl-25 me-xl-25 me-lg-15 me-md-2 header-logout btn btn-primary'}>
       <span className={'text-white me-2 fw-normal text-decoration-underline'}>Logout</span>
       <PcIcon name={'logout'} alt={'Logout'} className={'pc-icon-logout'} />
     </Button>
